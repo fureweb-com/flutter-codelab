9a81e52cf60191ad757844fe8912a8d90ce6d20f
fureweb-com <fureweb.com@gmail.com>
2018-12-14 22:28:37 +0900

    Update the build method for RandomWordsState to use _buildSuggestions
    - Prevent to call WordPair.random method directly in RandomWordsState
    - Use Scaffold Widget that implements the basic M.D visual layout
    - Set appBar and body in RandomWordsState

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -26,8 +26,12 @@ class RandomWordsState extends State<RandomWords> {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
   Widget _buildSuggestions() {
