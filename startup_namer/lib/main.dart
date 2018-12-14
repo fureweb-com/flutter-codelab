270549a64cba5299d05c23165002846e6ea52542
fureweb-com <fureweb.com@gmail.com>
2018-12-14 14:47:45 +0900

    Update lib/main file to apply english_words package

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
@@ -11,7 +13,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter!'),
         ),
         body: Center(
-          child: Text('Hello Fureweb!'),
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
