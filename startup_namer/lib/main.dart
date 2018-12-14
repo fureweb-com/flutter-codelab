70d276ca4766ccbb728b598f4539f64e28616a07
fureweb-com <fureweb.com@gmail.com>
2018-12-14 22:17:03 +0900

    Add a _buildSuggestions() function to the RandomWordsState class

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -24,10 +24,24 @@ class RandomWordsState extends State<RandomWords> {
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
-    Widget build(BuildContext context) {
-      final wordPair = WordPair.random();
-      return Text(wordPair.asPascalCase);
-    }
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 }
 
 class RandomWords extends StatefulWidget {
