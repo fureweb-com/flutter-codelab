0a69f07614d32689318e4627c7784f1454842605
fureweb-com <fureweb.com@gmail.com>
2018-12-16 01:44:08 +0900

    Implement the image section
    - add listView for image section
    - use image asset with key, height, fit property
    - append imageSection to home.body property of MaterialApp
    - remove unused classes

--- layout_example/lib/main.dart
+++ layout_example/lib/main.dart
@@ -90,35 +90,26 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
       ),
     );
 
+    Widget imageSection = ListView(
+      children: [
+        Image.asset(
+          'images/lake.jpg',
+          height: 240.0,
+          fit: BoxFit.cover,
+        ),
+      ],
+    );
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text('Hello World!'),
-      ),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Top Lakes'),
+          ),
+          body: imageSection,
+        ));
   }
 }
