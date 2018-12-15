473dbc1c2906bbaae63bf0756edbb1541a353fb8
fureweb-com <fureweb.com@gmail.com>
2018-12-16 01:30:39 +0900

    Implement the button row
    - create nested function to make button column

--- layout_example/lib/main.dart
+++ layout_example/lib/main.dart
@@ -46,6 +46,29 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
