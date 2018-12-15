3f7291f992302dfeacba334e15f927ef64b93662
fureweb-com <fureweb.com@gmail.com>
2018-12-15 18:10:45 +0900

    Update RandomWordsState class
    - add heart icon
    - add an alreadySaved variable in _buildRow method

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -43,10 +43,15 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
       ),
     );
   }
