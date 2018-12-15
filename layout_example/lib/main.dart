7715789a4c2852de91ac25dc9b4352a123d4d5b0
fureweb-com <fureweb.com@gmail.com>
2018-12-16 01:24:52 +0900

    Implement the title row
    - add widget for title section
    - structure: Row -> [Expanded, Icon, Text]
    - Expanded has 2 rows =>
    - first has padding so Container widget (look bad)
    - second has no padding so just Text widget

--- layout_example/lib/main.dart
+++ layout_example/lib/main.dart
@@ -11,6 +11,41 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -38,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Text('Hello World'),
+        child: Text('Hello World!'),
       ),
     );
   }
